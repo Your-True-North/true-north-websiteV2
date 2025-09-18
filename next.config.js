b2689f020632b06ae00c56800123e00bbfc935b8
Mase roberts <mase@Mases-MacBook-Air.local>
2025-09-18 22:12:28 +0800

    Add webpack alias for lib directory resolution

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/lib': path.resolve(__dirname, 'app/lib'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
