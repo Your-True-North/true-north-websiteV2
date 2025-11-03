0d3c61465bc4c7764d0cc06f200c76fe376c09b7
Mase roberts <mase@Mases-MacBook-Air.local>
2025-11-03 14:28:10 +0800

    update: circle page video link

--- app/(marketing)/circle/page.tsx
+++ app/(marketing)/circle/page.tsx
@@ -45,7 +45,7 @@ export default function Circle() {
     if (isPlaying && !playerRef.current && window.YT) {
       const initPlayer = () => {
         playerRef.current = new window.YT.Player('youtube-player', {
-          videoId: '-k7UOEJf9wM',
+          videoId: 'RAutFUW-erc',
           playerVars: {
             autoplay: 1,
             controls: 0,
