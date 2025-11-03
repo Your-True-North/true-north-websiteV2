b7b4bbabd4daedad110eef2534c288f37fa9e071
Mase roberts <mase@Mases-MacBook-Air.local>
2025-11-03 19:57:56 +0800

    Revert "update: work page video link"
    This reverts commit 4538462dafdf4e401dfabd2f95c433926e964c7b.

--- app/(marketing)/work/page.tsx
+++ app/(marketing)/work/page.tsx
@@ -58,7 +58,7 @@ export default function Work() {
     if (isPlaying && !playerRef.current && window.YT) {
       const initPlayer = () => {
         playerRef.current = new window.YT.Player('vsl-youtube-player', {
-          videoId: 'RAutFUW-erc',
+          videoId: '-k7UOEJf9wM',
           playerVars: {
             autoplay: 1,
             controls: 0,
