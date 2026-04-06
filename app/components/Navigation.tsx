b5e065facb4d7fc3be1df041ee2cfe6f4bfeeb3b
Your-True-North <navigate@yourtruenorth.me>
2026-04-06 20:32:13 +0800

    Update site nav logo to CoR mark SVG

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -71,13 +71,10 @@ export default function Navigation() {
      }}>
        <div className="nav-container">
          <Link href="/" className="nav-logo">
-           <Image
-             src="/white white star.png"
-             alt="True North"
-             width={72}
-             height={72}
-             className="nav-logo-image"
-             priority
+           <img
+             src="/cor-mark-black.svg"
+             alt="Circle of Return"
+             style={{ height: '52px', width: 'auto', display: 'block' }}
            />
          </Link>
 
